2bbf68390939aec44d45a3c17cd1b324e527bfe5
Hugo How-Choong <hugohowchoong@gmail.com>
2022-09-08 17:04:04 +0200

    Add auth and configuration

--- lib/sowprog-api-ruby.rb
+++ lib/sowprog-api-ruby.rb
@@ -4,22 +4,31 @@ require 'base64'
 require 'json'
 
 class SowprogApiRuby
-    def self.hi
-        puts "Hello world!"
+    class Configuration
+        attr_accessor :user_name, :password
     end
 
     def self.scheduled_events
-        puts "scheduled_events!"
         url = URI('https://agenda.sowprog.com/rest/v1_2/scheduledEvents/search')
         request = Net::HTTP::Get.new(url)
         request["Accept"] = "application/json"
         request["Content-Type"] = "application/json"
-        request["Authorization"] =  "Basic " + Base64.encode64("user:password").strip
+        request["Authorization"] =  "Basic " + Base64.encode64("#{SowprogApiRuby.configuration.user_name}:#{SowprogApiRuby.configuration.password}").strip
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         response = http.request request
         return JSON.parse(response.body)
     end
+
+
+    def self.configuration
+        @configuration ||= Configuration.new
+    end
+
+    def self.configure(&block)
+        yield(configuration)
+    end      
+
 end
 
 # SowprogApiRuby::ScheduledEvents.fetch
